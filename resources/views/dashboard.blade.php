6c648f274ec64f656916c4954f0eed9f40f21d7d
santosinarpandean <santosinar.bitcoin@gmail.com>
2017-10-04 07:09:03 -0400

    REVISI

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.master')
 @section('title','Trans | Dashboard')
 @section('content')
+
     <!-- page content -->
     <div class="right_col" role="main">
         <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12 col-md-offset-3" style="margin-top:20%">
+                <div class="title m-b-md">
+                    <h3>Selamat Datang di PT. Nomina VIP Indonesia</h3>
+                </div>
+            </div>
+
 
         </div>
 
